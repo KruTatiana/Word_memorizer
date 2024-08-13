1e8869dac54c9140d2af92989f5ff8cd9d2ad9dc
KruTatiana <ilm63.rus@gmail.com>
2024-08-14 00:32:51 +0300

    configs

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
       "/api": {
-        target: "http://itgirlschool.justmakeit.ru",
+        target: "https://itgirlschool.justmakeit.ru",
         changeOrigin: true,
       },
     },
   },
+  base: '/Word_memorizer'
 });
